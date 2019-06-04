3184bf969164ddf3322522d1c754b186dbcd44ad
frank <francidalca@gmail.com>
2019-06-04 13:16:37 +0200

    pushing for joseph

--- compare.py
+++ compare.py
@@ -7,13 +7,15 @@ file_name = 'aligned_epg_transcriptions_npo1_npo2.csv'
 data = pd.read_csv(file_name)
 texts = data['text']
 
-text = texts[0]
+n = 8
 
-n = 5
-tfidf_words = tfidf(text, n=n)
-rake_words = rake(text, n=n)
-yake_words = yake_keywords(text, n=n)
+for text in texts[0:5]:
+    tfidf_words = tfidf(text, n=n)
+    rake_words = rake(text, n=n)
+    yake_words = yake_keywords(text, n=n)
 
-print("index. tf-idf | rake | yake")
-for idx in range(n):
-    print(f"{idx + 1}) {tfidf_words[idx]} | {rake_words[idx]} | {yake_words[idx]}")
+    print()
+    print("index. tf-idf | rake | yake")
+    for idx in range(n):
+        if len(tfidf_words)
+        print(f"{idx + 1}) {tfidf_words[idx]} | {rake_words[idx]} | {yake_words[idx]}")
