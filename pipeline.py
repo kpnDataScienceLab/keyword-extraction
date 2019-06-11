d3e604072b89a85e42b07bb6c2bbeef2300216e1
Joseph <josephgk@hotmail.nl>
2019-06-11 16:47:50 +0200

    pipeline

--- pipeline.py
+++ pipeline.py
@@ -4,7 +4,7 @@ from datasets.datasets import Dataset
 from eval_metrics import mean_ap, f1
 
 dataset = Dataset()
-train(dataset.get_texts())
+train([t[0] for t in dataset.get_texts()])
 
 for (text,target) in dataset:
 	result = test(text)
