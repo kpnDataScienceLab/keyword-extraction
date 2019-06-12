a5c475f963122ca02dda85d200b5da8b90554120
Joseph <josephgk@hotmail.nl>
2019-06-12 11:42:15 +0200

    add args

--- tfidf/tfidf.py
+++ tfidf/tfidf.py
@@ -1,10 +1,8 @@
 import nltk
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-nltk.download('stopwords')
 
 global _model
-
 
 def get_top_n_tfidf(response, feature_names, n=5):
     d = {}
@@ -13,7 +11,8 @@ def get_top_n_tfidf(response, feature_names, n=5):
     return sorted(d, key=lambda key: d[key], reverse=True)[:n]
 
 
-def train(dataset,lang='dutch'):
+def train(dataset,arguments,lang='dutch'):
+    nltk.download('stopwords')
     global _model
     stopwords = nltk.corpus.stopwords.words(lang)
     _model = TfidfVectorizer(stop_words=stopwords, ngram_range=(1, 3))
@@ -21,7 +20,7 @@ def train(dataset,lang='dutch'):
 
 
 
-def test(text, n=5):
+def test(text,arguments, n=5):
     response = _model.transform([text])
     feature_names = _model.get_feature_names()
     return get_top_n_tfidf(response, feature_names, n)
