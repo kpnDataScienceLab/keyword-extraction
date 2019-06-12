4f4ed5c96cc1e90e217a338e65494b7789782b3d
Arvid <Arvid.Lindstrom@gmail.com>
2019-06-12 14:17:44 +0200

    Prepared PKE models for testing

--- git_pke/pke_models.py
+++ git_pke/pke_models.py
@@ -2,8 +2,19 @@ import pke
 import nltk
 import string
 from pke import compute_document_frequency
-from cleandata import readCleanTranscript
+# from cleandata import readCleanTranscript
 nltk.download('stopwords')
+import json
+
+# Comment out following code before pushing
+# import os,sys,inspect
+# currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+# parentdir = os.path.dirname(currentdir)
+# sys.path.insert(0,parentdir) 
+
+# from eval_metrics import average_precision
+# from datasets.datasets import Dataset
+# -----------------------------------------end
 
 
 """
@@ -22,14 +33,14 @@ python -m spacy download en
 To test models use the following interface:
 
 
-from git_pke import pke_yake, pke_textRank, pke_singleRank, \
+from git_pke.pke_models import pke_yake, pke_textRank, pke_singleRank, \
 	pke_topicRank, pke_positionRank, pke_multipartieRank
 
-## Functions are called as 
-topN = pke_yake(text, n = 5, language = 'nl')
+## Functions are called as, 
+topN = pke_yake(text, n = 5, language = 'nl') # or 'en'
 topN = pke_textRank(text, n = 5, language = 'nl')
 topN = pke_singleRank(text, n = 5, language = 'nl')
-topN = pke_topicRank(text, n = 5, language = 'dutch')
+topN = pke_topicRank(text, n = 5, language = 'dutch') # or 'english'
 topN = pke_positionRank(text, n = 5, language = 'nl')
 topN = pke_multipartieRank(text, n = 5, language = 'dutch')
 
@@ -145,10 +156,6 @@ def pke_multipartieRank(text, n = 5, language = 'dutch'):
 def pke_wingnus(text, n = 5, language = 'nl'):
 	pass
 
-
-
-
-
 # Low priority models
 
 # TfIdf
@@ -164,19 +171,46 @@ def pke_kpminer(text, n = 5, language = 'nl'):
 
 if __name__ == '__main__':
 
-	transcript = ' '.join(readCleanTranscript(
-		"clean_transcripts_june11.txt", 
-		4))
+	# Use this for Dutch testing on TV data
+	# transcript = ' '.join(readCleanTranscript(
+	# 	"clean_transcripts_june11.txt", 
+	# 	4))
 
+	data = Dataset("500N-KPCrowd")
+
+	path = "../datasets/ake-datasets/datasets/500N-KPCrowd/train/"
+	filename = "art_and_culture-20893614.xml"
+	path = path + filename
+
+	transcript = data.parse_xml(path)
+	language1 = 'en'		# nl
+	language2 = 'english' 	# dutch
+	print("Input:\n\n")
+	print(transcript)
+
+	labels_path = "../datasets/ake-datasets/datasets/500N-KPCrowd/references/"
+	lfile = "train.reader.json"
+	with open(labels_path + lfile) as handle:
+		l = json.load(handle)	
+	labels = l[filename[:-4]]
+	for idx in range(len(labels)):
+		labels[idx] = labels[idx][0]
+
+
+	print("\n\nPredictions: ")
 	print("Running yake:")
-	print(pke_yake(transcript, n = 10, language = 'nl'))
+	print(pke_yake(path, n = 10, language = language1))
 	print("Running textRank:")
-	print(pke_textRank(transcript, n = 10, language = 'nl'))
+	print(pke_textRank(path, n = 10, language = language1))
 	print("Running singleRank:")
-	print(pke_singleRank(transcript, n = 10, language = 'nl'))
+	print(pke_singleRank(path, n = 10, language = language1))
 	print("Running topicRank:")
-	print(pke_topicRank(transcript, n = 10, language = 'dutch'))
+	print(pke_topicRank(path, n = 10, language = language2))
 	print("Running positionRank:")
-	print(pke_positionRank(transcript, n = 10, language = 'nl'))
+	print(pke_positionRank(path, n = 10, language = language1))
 	print("Running MultipartiteRank:")
-	print(pke_multipartieRank(transcript, n = 10, language = 'dutch'))
+	print(pke_multipartieRank(path, n = 10, language = language2))
+
+	print("\n\nLabels: ", labels)
+
+	print("Scores:")
