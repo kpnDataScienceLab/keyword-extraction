18e6dbefc93d3f2516eee0d2b0cd436d01bccaf4
Arvid <Arvid.Lindstrom@gmail.com>
2019-06-12 10:22:28 +0200

    not much

--- git_pke/pke_models.py
+++ git_pke/pke_models.py
@@ -1,3 +1,5 @@
+
+# This will be commented out before pushing
 import os,sys,inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
